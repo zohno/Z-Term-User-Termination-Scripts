56734b4cce03f9ebb8eefd18410515190dd5679b
denny@zohno.com <denny@zohno.com>
2016-11-16 11:30:41 +0530

    Added 2 scripts
    Both scripts need to be combine yet

---
+++ Scripts/Zendesk/AllPropertiesOfUserBySamAccountNameLatest.ps1
@@ -0,0 +1,78 @@
+﻿$user = args[0]
+$pass= args[1]
+$SamAccountName = args[2]
+$pair = "$($user):$($pass)"
+
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+
+$basicAuthValue = "Basic $encodedCreds"
+
+$Headers = @{
+    Authorization = $basicAuthValue
+}
+
+$Result = Invoke-WebRequest -Uri 'https://zohno.zendesk.com/api/v2/search.json?query=type:user "Balram"' -Headers $Headers
+
+
+$x = $Result.Content | ConvertFrom-Json
+$x.results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Scripts/Zendesk/SuspendZendeskUser.ps1
@@ -0,0 +1,102 @@
+﻿$user = args[0]
+$pass= args[1]
+$SamAccountName = args[2]
+$pair = "$($user):$($pass)"
+
+$encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+
+$basicAuthValue = "Basic $encodedCreds"
+
+$Headers = @{
+    Authorization = $basicAuthValue
+}
+
+$Result = Invoke-WebRequest -Uri "https://zohno.zendesk.com/api/v2/users/6291759845.json" -body '{"user": {"suspended": true}}' -ContentType "application/json" -Headers $headers -Method PUT
+
+$Result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
